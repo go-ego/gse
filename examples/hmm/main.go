589ef8278b51efcf5378eb722b21c6f0b4f2d100
vcaesar <vzvway@gmail.com>
2019-01-23 19:10:58 -0400

    add hmm example code

--- examples/hmm/main.go
+++ examples/hmm/main.go
@@ -6,10 +6,24 @@ import (
 	"github.com/go-ego/gse"
 )
 
-var seg gse.Segmenter
+var (
+	text = "纽约时代广场, 纽约帝国大厦"
+
+	seg gse.Segmenter
+)
 
 func main() {
-	hmm := seg.HMMCutMod("纽约时代广场")
-
+	hmm := seg.HMMCutMod(text)
 	fmt.Println("hmm cut: ", hmm)
+
+	seg.LoadDict()
+
+	hmm = seg.Cut(text, true)
+	fmt.Println("hmm cut: ", hmm)
+
+	hmm = seg.CutSearch(text, true)
+	fmt.Println("hmm cut: ", hmm)
+
+	hmm = seg.CutAll(text)
+	fmt.Println("cut all: ", hmm)
 }
