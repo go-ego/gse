74c77418a15515ee687942e89b6145ada3c6b005
vcaesar <vzvway@gmail.com>
2019-03-17 09:26:36 -0400

    update godoc

--- hmm/hmm_seg.go
+++ hmm/hmm_seg.go
@@ -12,7 +12,7 @@
 // License for the specific language governing permissions and limitations
 // under the License.
 
-// Package hmm is the Golang HMM cut module
+// package hmm is the Golang HMM cut module
 
 package hmm
 
