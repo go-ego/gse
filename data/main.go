b7d97776f3a8036b29667c05a29e9a53c029d087
vcaesar <vzvway@gmail.com>
2019-05-28 14:08:26 -0400

    update data re base code

--- data/main.go
+++ data/main.go
@@ -8,15 +8,33 @@ import (
 )
 
 var (
-	text = flag.String("text", "《复仇者联盟3：无限战争》是全片使用IMAX摄影机拍摄", "要分词的文本")
+	text     = "《复仇者联盟3：无限战争》是全片使用IMAX摄影机拍摄"
+	flagText = flag.String("text", text, "要分词的文本")
 )
 
 func main() {
 	flag.Parse()
 
 	var seg gse.Segmenter
-	seg.LoadDict("./data/dict/dictionary.txt")
+	// seg.LoadDict("./data/dict/dictionary.txt")
+	seg.LoadDict()
 
-	segments := seg.Segment([]byte(*text))
+	// use DAG and HMM
+	hmm := seg.Cut(text, true)
+	fmt.Println("cut use hmm: ", hmm)
+	//
+	cut := seg.Cut(text)
+	fmt.Println("cut: ", cut)
+
+	hmm = seg.CutSearch(text, true)
+	fmt.Println("cut search use hmm: ", hmm)
+	//
+	cut = seg.CutSearch(text)
+	fmt.Println("cut search: ", hmm)
+
+	cut = seg.CutAll(text)
+	fmt.Println("cut all: ", cut)
+
+	segments := seg.Segment([]byte(*flagText))
 	fmt.Println(gse.ToString(segments, true))
 }
