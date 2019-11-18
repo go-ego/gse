be0ec7e2ac02804d039c564708a0b21d57274645
vcaesar <vzvway@gmail.com>
2019-11-18 13:52:49 -0400

    add more benchmark support

---
+++ gse_bm_test.go
@@ -0,0 +1,37 @@
+package gse
+
+import (
+	"testing"
+
+	"github.com/vcaesar/tt"
+)
+
+var text = "纽约时代广场, 纽约帝国大厦, 旧金山湾金门大桥"
+
+func init() {
+	prodSeg.LoadDict()
+}
+
+func BenchmarkCut(t *testing.B) {
+	fn := func() {
+		prodSeg.Cut(text)
+	}
+
+	tt.BM(t, fn)
+}
+
+func BenchmarkCutAll(t *testing.B) {
+	fn := func() {
+		prodSeg.CutAll(text)
+	}
+
+	tt.BM(t, fn)
+}
+
+func BenchmarkCutSearch(t *testing.B) {
+	fn := func() {
+		prodSeg.CutSearch(text)
+	}
+
+	tt.BM(t, fn)
+}
