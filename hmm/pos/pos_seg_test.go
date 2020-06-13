37e4e3ee56316c3742f576e38ff7a8261998b727
vcaesar <vzvway@gmail.com>
2020-06-13 11:58:43 -0400

    add pos seg test code

--- hmm/pos/pos_seg_test.go
+++ hmm/pos/pos_seg_test.go
@@ -1,1 +1,18 @@
 package pos
+
+import (
+	"testing"
+
+	"github.com/vcaesar/tt"
+)
+
+var (
+	seg Segmenter
+)
+
+func TestCut(t *testing.T) {
+	seg.LoadDict()
+
+	s := seg.Cut("那里湖面总是澄清, 那里空气充满宁静", true)
+	tt.Equal(t, "[{那里 r} {湖面 n} {总是 c} {澄清 v} {, x} {  x} {那里 r} {空气 n} {充满 a} {宁静 nr}]", s)
+}
