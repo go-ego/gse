b1be523279de8143a48f39b858d8d34d26ee136b
vcaesar <vzvway@gmail.com>
2020-06-19 23:49:45 -0400

    add more test code

--- gse_test.go
+++ gse_test.go
@@ -68,9 +68,9 @@ func TestPos(t *testing.T) {
 	tt.Equal(t,
 		"[{纽约 ns} {时代 n} {广场 n} {时代广场 n} {纽约时代广场 nt} {纽约 ns} {帝国 n} {大厦 n} {帝国大厦 nr} {金山 nr} {旧金山 ns} {湾 zg} {旧金山湾 ns} {金门 n} {大桥 ns} {金门大桥 nz}]", pos)
 
-	pos = prodSeg.TrimPos(pos, "n")
-	tt.Equal(t, 10, len(pos))
+	pos = prodSeg.TrimPos(pos, "n", "zg")
+	tt.Equal(t, 9, len(pos))
 	tt.Equal(t,
-		"[{纽约 ns} {纽约时代广场 nt} {纽约 ns} {帝国大厦 nr} {金山 nr} {旧金山 ns} {湾 zg} {旧金山湾 ns} {大桥 ns} {金门大桥 nz}]", pos)
+		"[{纽约 ns} {纽约时代广场 nt} {纽约 ns} {帝国大厦 nr} {金山 nr} {旧金山 ns} {旧金山湾 ns} {大桥 ns} {金门大桥 nz}]", pos)
 
 }
--- hmm/pos/pos_seg_test.go
+++ hmm/pos/pos_seg_test.go
@@ -10,9 +10,11 @@ var (
 	seg Segmenter
 )
 
-func TestCut(t *testing.T) {
+func init() {
 	seg.LoadDict()
+}
 
+func TestCut(t *testing.T) {
 	s := seg.Cut("那里湖面总是澄清, 那里空气充满宁静", true)
 	tt.Equal(t, "[{那里 r} {湖面 n} {总是 c} {澄清 v} {, x} {  x} {那里 r} {空气 n} {充满 a} {宁静 nr}]", s)
 
@@ -24,4 +26,8 @@ func TestCut(t *testing.T) {
 	s = seg.Cut(text, false)
 	tt.Equal(t,
 		"[{工信处 n} {女干事 n} {每月 r} {经过 p} {下属 v} {科室 n} {都 d} {要 v} {亲口 n} {交代 n} {24 eng} {口 q} {交换 v} {机 n} {等 u} {技术 n} {性 n} {器件 n} {的 uj} {安装 v} {工作 vn}]", s)
+
+	pos := seg.TrimPos(s, "n", "r", "uj")
+	tt.Equal(t, 10, len(pos))
+	tt.Equal(t, "[{经过 p} {下属 v} {都 d} {要 v} {24 eng} {口 q} {交换 v} {等 u} {安装 v} {工作 vn}]", pos)
 }
