fe23a0873847e9d54e52445179abf7dc89fc8977
vcaesar <vzvway@gmail.com>
2020-07-05 02:50:19 -0400

    update test code

--- gse_test.go
+++ gse_test.go
@@ -30,9 +30,9 @@ func TestHMM(t *testing.T) {
 	tt.Equal(t, 6, len(tx))
 	tt.Equal(t, "[纽约时代广场 纽约 帝国大厦 旧金山湾 金门大桥  广场]", tx)
 
-	s := prodSeg.CutStr(tx)
-	tt.Equal(t, 72, len(s))
-	tt.Equal(t, "纽约时代广场 纽约 帝国大厦 旧金山湾 金门大桥  广场", s)
+	s := prodSeg.CutStr(tx, ", ")
+	tt.Equal(t, 77, len(s))
+	tt.Equal(t, "纽约时代广场, 纽约, 帝国大厦, 旧金山湾, 金门大桥,  广场", s)
 
 	tx = prodSeg.CutAll(text)
 	tt.Equal(t, 21, len(tx))
@@ -82,10 +82,10 @@ func TestPos(t *testing.T) {
 	tt.Equal(t,
 		"[{纽约 ns} {时代 n} {广场 n} {时代广场 n} {纽约时代广场 nt} {纽约 ns} {帝国 n} {大厦 n} {帝国大厦 nr} {金山 nr} {旧金山 ns} {湾 zg} {旧金山湾 ns} {金门 n} {大桥 ns} {金门大桥 nz}]", pos)
 
-	s = prodSeg.PosStr(pos)
-	tt.Equal(t, 189, len(s))
+	s = prodSeg.PosStr(pos, ", ")
+	tt.Equal(t, 204, len(s))
 	tt.Equal(t,
-		"纽约/ns 时代/n 广场/n 时代广场/n 纽约时代广场/nt 纽约/ns 帝国/n 大厦/n 帝国大厦/nr 金山/nr 旧金山/ns 湾/zg 旧金山湾/ns 金门/n 大桥/ns 金门大桥/nz", s)
+		"纽约/ns, 时代/n, 广场/n, 时代广场/n, 纽约时代广场/nt, 纽约/ns, 帝国/n, 大厦/n, 帝国大厦/nr, 金山/nr, 旧金山/ns, 湾/zg, 旧金山湾/ns, 金门/n, 大桥/ns, 金门大桥/nz", s)
 
 	pos = prodSeg.TrimPos(pos, "n", "zg")
 	tt.Equal(t, 9, len(pos))
