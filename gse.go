2c54c7ce9b0e57d12e2c7653bbc29c64532bcc67
vcaesar <vzvway@gmail.com>
2020-08-25 12:07:19 -0400

    Released v0.60.0 Green Lake

--- gse.go
+++ gse.go
@@ -24,7 +24,7 @@ import (
 
 const (
 	// Version get the gse version
-	Version = "v0.60.1.444-rc3, Nisqually Glacier!"
+	Version = "v0.60.0.497, Green Lake!"
 
 	// minTokenFrequency = 2 // 仅从字典文件中读取大于等于此频率的分词
 )
