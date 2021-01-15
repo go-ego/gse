0446abbfe79de41e237570fc84ca8f308758cd5e
vcaesar <vzvway@gmail.com>
2021-01-15 11:52:27 -0400

    update CutUrl and add split num function

--- trim.go
+++ trim.go
@@ -16,6 +16,7 @@ package gse
 
 import (
 	"regexp"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
@@ -171,7 +172,8 @@ func (seg *Segmenter) CutTrimHtmls(str string, hmm ...bool) string {
 
 // CutUrl cut url string trim symbol return []string
 func (seg *Segmenter) CutUrl(str string) []string {
-	seg.Num = true
+	// seg.Num = true
+	str = SplitNums(str)
 	s := seg.Cut(str)
 	return seg.TrimSymbol(s)
 }
@@ -179,6 +181,17 @@ func (seg *Segmenter) CutUrl(str string) []string {
 // CutUrls cut url string trim symbol return string
 func (seg *Segmenter) CutUrls(str string) string {
 	return seg.CutStr(seg.CutUrl(str), " ")
+}
+
+// SplitNum cut string by num to []string
+func SplitNum(text string) []string {
+	r := regexp.MustCompile(`\d+|\D+`)
+	return r.FindAllString(text, -1)
+}
+
+// SplitNums cut string by num to string
+func SplitNums(text string) string {
+	return strings.Join(SplitNum(text), " ")
 }
 
 // FilterEmoji filter the emoji
