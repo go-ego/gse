ad5f28d40cd6f4a9e72ce8454b4dfdc6f36a54eb
vcaesar <vzvway@gmail.com>
2021-09-02 11:14:18 -0400

    Add more embed function and test code

--- dict_1.16_test.go
+++ dict_1.16_test.go
@@ -13,11 +13,16 @@ func TestLoadDictEmbed(t *testing.T) {
 	// err := seg1.LoadDictEmbed()
 	// tt.Nil(t, err)
 
-	seg1 := NewEmbed("en")
+	seg1, err := NewEmbed("zh, world 20 n", "en")
+	tt.Nil(t, err)
 
 	f, ok := seg1.Find("1号店")
 	tt.Bool(t, ok)
 	tt.Equal(t, 3, f)
+
+	f, ok = seg1.Find("world")
+	tt.Bool(t, ok)
+	tt.Equal(t, 20, f)
 
 	f, ok = seg1.Find("八千一百三十七万七千二百三十六口")
 	tt.Bool(t, ok)
